6f82b76bce45dec09957809417c5d7d8ab0a08b5
Ubitroll <stephencartner@hotmail.co.uk>
2019-11-08 09:15:06 +0000

    set up

---
+++ Lab4/Lab4/Lab1/MainGame.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <SDL\SDL.h>
+#include <GL/glew.h>
+#include "Display.h" 
+#include "Shader.h"
+#include "Mesh.h"
+#include "Texture.h"
+#include "transform.h"
+
+enum class GameState{PLAY, EXIT};
+
+class MainGame
+{
+public:
+	MainGame();
+	~MainGame();
+
+	void run();
+
+private:
+
+	void initSystems();
+	void processInput();
+	void gameLoop();
+	void drawGame();
+
+	Display _gameDisplay;
+	GameState _gameState;
+	Mesh mesh1;
+	Mesh mesh2;
+	Camera myCamera;
+	Texture texture; 
+	Shader shader;
+
+
+	float counter;
+
+
+};
+
